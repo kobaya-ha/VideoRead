133cb76a574773b4c1a91a898cf9089aee6f4a92
kobaya-ha <k1630048@edu.cc.uec.ac.jp>
2016-10-22 20:30:52 +0900

    make c3dnet.py and remove MyChain() in train.py

---
+++ c3dnet.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+import chainer
+import chainer.functions as F
+from chainer import initializers
+import chainer.links as L
+
+
+class C3D(chainer.Chain):
+
+    """Convolution3Dnet"""
+
+    #insize = 227
+
+    def __init__(self):
+        super(C3D, self).__init__(
+            conv1=L.ConvolutionND(3,3,10,ksize = (3,3,3), stride= (1,3,3), pad = 0),
+            conv2=L.ConvolutionND(3,10,15,ksize=5, stride=(1,2,2), pad = 0),
+            conv3=L.ConvolutionND(3,15,15,ksize=3, stride=2, pad = 0),
+            fc4=L.Linear(33750, 4096),
+            fc5=L.Linear(4096, 4096),
+            fc6=L.Linear(4096, 102),
+        )
+        self.train = True
+
+    def __call__(self, x, t):
+        h = F.relu(self.conv1(x))
+        h = F.relu(self.conv2(h))
+        h = F.relu(self.conv3(h))
+        h = self.fc4(h)
+        h = self.fc5(h)
+        h = self.fc6(h)
+        
+
+        loss = F.softmax_cross_entropy(h, t)
+        chainer.report({'loss': loss, 'accuracy': F.accuracy(h, t)}, self)
+        return loss
--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import chainer.functions as F
 import videoread
 from chainer import optimizers, Variable, serializers, training
 from chainer.training import extensions
-
+import c3dnet
 
 #from sklearn.cross_validation import train_test_split
 
@@ -22,7 +22,7 @@ data, label = v.makelist_all_class(sys.argv[1])
 #label = chainer.cuda.to_gpu(label)
 
 """
-バイナリから呼び出す時の記述
+バイナリから呼び出す時の記述 //たぶん書き換えないと動きません
 data = np.empty((0,18,240,320,3), dtype=np.float32)
 for file in filelist:
 	print  file
@@ -31,86 +31,51 @@ for file in filelist:
 		data = np.append(data, single_data["data"])
 """
 
+def main():
 
 
-class MyChain(chainer.Chain):
-	def __init__(self):
-		super(MyChain, self).__init__(
-			conv1=L.ConvolutionND(3,3,10,ksize = (3,3,3), stride= (1,3,3), pad = 0),
-			conv2=L.ConvolutionND(3,10,15,ksize=5, stride=(1,2,2), pad = 0),
-			conv3=L.ConvolutionND(3,15,15,ksize=3, stride=2, pad = 0),
-			#conv4=L.ConvolutionND(3,15, 15,ksize=3, stride=2, pad = 0),
-			fc5=L.Linear(33750, 4096),
-			fc6=L.Linear(4096, 4096),
-			fc7=L.Linear(4096, 102),
-		)
-		self.train = True
+	model = c3dnet.C3D()
 
-	def __call__(self, x, t):
-		#x = Variable(x)
-		#t = Variable(t)
+	#cuda.get_device(0).use()
+	#model.to_gpu()
 
-		print x.data.shape
-		h = self.conv1(x)
-		print h.data.shape
-		h = self.conv2(h)
-		print h.data.shape
-		h = self.conv3(h)
-		print h.data.shape
-		#h = self.conv4(h)
-		#print h.data.shape
-		h = self.fc5(h)
-		print h.data.shape
-		h = self.fc6(h)
-		print h.data.shape
-		h = self.fc7(h)
-		print h.data.shape
-		#print h.data
-		
-		self.loss = F.softmax_cross_entropy(h,t)
-		self.accuracy = F.accuracy(h,t)
-		return self.loss
-
-
-model = MyChain()
-
-#cuda.get_device(0).use()
-#model.to_gpu()
-
-optimizer = optimizers.Adam()
-optimizer.setup(model)
-#x_train, x_test, y_train, y_test = train_test_split(data, label, test_size =0.1)
+	optimizer = optimizers.Adam()
+	optimizer.setup(model)
+	#x_train, x_test, y_train, y_test = train_test_split(data, label, test_size =0.1)
 
 
 
-n = 102 #データサイズ
-bs = 10 #ミニバッチサイズ
+	n = 102 #データサイズ
+	bs = 10 #ミニバッチサイズ
 
-for i in range(20):
-	sffindx = np.random.permutation(n)
+	for i in range(20):
+		sffindx = np.random.permutation(n)
 
-	#cupyindx = xp.array(sffindx, xp.int32)
-	#cupyindx = cupyindx.astype(xp.int32)
+		#cupyindx = xp.array(sffindx, xp.int32)
+		#cupyindx = cupyindx.astype(xp.int32)
 
-	#print type(cupyindx[0])
-	for j in range(0, n, bs):
-		#print cupyindx[j:(j+bs) if (j+bs) < n else n]
-		data_b = Variable(data[sffindx[j:(j+bs) if (j+bs) < n else n]])
-		label_b = Variable(label[sffindx[j:(j+bs) if (j+bs) < n else n]])
-		model.zerograds()
-		loss = model(data_b, label_b)
-		loss.backward()
-		optimizer.update()
-		print loss.data
-"""
+		#print type(cupyindx[0])
+		for j in range(0, n, bs):
+			#print cupyindx[j:(j+bs) if (j+bs) < n else n]
+			data_b = Variable(data[sffindx[j:(j+bs) if (j+bs) < n else n]])
+			label_b = Variable(label[sffindx[j:(j+bs) if (j+bs) < n else n]])
+			model.zerograds()
+			loss = model(data_b, label_b)
+			loss.backward()
+			optimizer.update()
+			print loss.data
+	"""
 
-train_iter = chainer.iterators.MultiprocessIterator(data, bs)
+	train_iter = chainer.iterators.MultiprocessIterator(data, bs)
 
-#set up a trainer
-updater = training.StandardUpdater(train_iter, optimizer)
-trainer = training.Trainer(updater, (10, 'epoch'), "result")
+	#set up a trainer
+	updater = training.StandardUpdater(train_iter, optimizer)
+	trainer = training.Trainer(updater, (10, 'epoch'), "result")
 
 
-trainer.extend(extensions.ProgressBar(update_interval=10))
-serializers.save_npz('my.model', model)
-"""
+	trainer.extend(extensions.ProgressBar(update_interval=10))
+	serializers.save_npz('my.model', model)
+	"""
+
+if __name__ == '__main__':
+    main()
